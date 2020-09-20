8bb3ec11c6be3192ad2052f4ceb4f101fa1479a2
israakittaneh <israakittanehm@gmail.com>
2020-09-20 18:01:02 +0300

    new2

--- scripit.js
+++ scripit.js
@@ -1,18 +1,58 @@
-//check when click on equal button if empty and give us alert 
-$(document).ready(function(){
-	$('#equal').click(function(){
-		if($('#screen1').val('')){
-			alert("Plese Entere number");
-		}
-	})
-});
+// //check when click on equal button if empty and give us alert 
+// $(document).ready(function(){
+// 	$('#equal').click(function(){
+// 		if($('#screen1').val('')){
+// 			alert("Plese Entere number");
+// 		}
+// 	})
+// });
 
 
 
-function insert(num){
-
-	calc.display.value +=num;
+function getinput(num){
+var x = num;
+if(x === '='){
+document.getElementById('screen1').value= eval(document.getElementById('screen1').value);
+}else if(x === 'c'){
+	document.getElementById('screen1').value ='';
 }
-function result(){
-	calc.display.value = eval(calc.display.value)
+else{
+	document.getElementById('screen1').value+=num;
 }
+	
+
+}
+
+
+
+// var pre = 0;
+// var precalc ;
+// var c= false;
+// var flag = false;
+// var screen =function (par){
+// 	if(par == '.' && flag == true){
+// 		return ;
+// 	}
+// 	if(c ==true){
+// 		$("#screen").val('');
+// 		c=false;
+// 	}
+// 	var r = $('#screen').val('')+par;
+// 	if(par == '.'){
+// 		flag = true;
+// 	}else{
+// 		r = r * 1;
+// 	}
+// 	$('#screen').val('');
+// }
+
+// var calculate = function(par){
+// 	if(pre){
+// 		result();
+// 	}
+// 	flag = false;
+// 	c =true;
+// 	pre = $('#screen').val('');
+// 	precalc = par;
+// 	}
+
